e8cd109e72d925b91cd7bad0eb5bd70a2ce711b6
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-01 12:26:49 -0300

    att

--- goallive/src/App.tsx
+++ goallive/src/App.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Games from "./pages/Games";
-import Standings from "./pages/Standings";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/games" replace />} />
-        <Route path="/games" element={<Games />} />
-        <Route path="/standings/:leagueId" element={<Standings />} />
-      </Routes>
-    </Router>
+
   );
 };
 
