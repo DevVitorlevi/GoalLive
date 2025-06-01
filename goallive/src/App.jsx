9b108e1080c7ab1a7cb5c5a2c1f42c77fb81476d
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-06-01 19:57:09 -0300

    ATT

--- goallive/src/App.jsx
+++ goallive/src/App.jsx
@@ -1,20 +1,9 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/Global';
-import { darkTheme } from './styles/theme';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Standings from './pages/Standings';
+
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tabela" element={<Standings />} />
-      </Routes>
-    </ThemeProvider>
-  );
+  
+  )
 }
 
-export default App;
+export default App
