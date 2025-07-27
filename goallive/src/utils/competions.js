05e4b352162ca4ce062dcdcf60743a3e5dc99d1a
Vitor Levi <levivitor8@gmail.com>
2025-07-27 14:44:00 -0300

    att

--- goallive/src/utils/competions.js
+++ goallive/src/utils/competions.js
@@ -2,16 +2,12 @@ export const COMPETITIONS = {
   // Ligas Europeias
   39: { name: 'Premier League', country: 'England', type: 'league', priority: 1 },
   140: { name: 'La Liga', country: 'Spain', type: 'league', priority: 1 },
-  135: { name: 'Serie A', country: 'Italy', type: 'league', priority: 1 },
   78: { name: 'Bundesliga', country: 'Germany', type: 'league', priority: 1 },
   61: { name: 'Ligue 1', country: 'France', type: 'league', priority: 1 },
 
   // Ligas das Américas
   71: { name: 'Brasileirão Série A', country: 'Brazil', type: 'league', priority: 1 },
-  72: { name: 'Brasileirão Série B', country: 'Brazil', type: 'league', priority: 2 },
 
-  // Liga Ásia/África
-  307: { name: 'Saudi Pro League', country: 'Saudi Arabia', type: 'league', priority: 3 },
 
   // Competições Europeias
   2: { name: 'UEFA Champions League', country: 'Europe', type: 'cup', priority: 1 },
@@ -21,8 +17,6 @@ export const COMPETITIONS = {
 
   // Competições Internacionais
   1: { name: 'Copa do Mundo', country: 'International', type: 'cup', priority: 1 },
-  4: { name: 'Eurocopa', country: 'Europe', type: 'cup', priority: 1 },
-  9: { name: 'Copa América', country: 'South America', type: 'cup', priority: 1 },
 };
 
 export const ALLOWED_COMPETITION_IDS = Object.keys(COMPETITIONS).map(Number);
