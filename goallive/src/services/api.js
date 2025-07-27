b94b8406966c129367d436222c28c12d3e932743
Vitor Levi <levivitor8@gmail.com>
2025-07-27 14:31:58 -0300

    att

--- goallive/src/services/api.js
+++ goallive/src/services/api.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = '9c08267e4c40fc635cb4e89303d336a0';
 const BASE_URL = 'https://v3.football.api-sports.io';
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'x-rapidapi-key': API_KEY,
-    'x-rapidapi-host': 'v3.football.api-sports.io',
-  },
+    'x-apisports-key': import.meta.env.VITE_API_KEY
+  }
 });
 
 export default api;
--- goallive/vite.config.js
+++ goallive/vite.config.js
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.football-data.org/v4',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq) => {
-            proxyReq.setHeader('X-Auth-Token', '69e8cbbede3c4f0c86c69b5e2fb899c0');
-          });
-        }
-      }
-    }
-  }
+  plugins: [react()]
 });
